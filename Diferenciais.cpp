f5441dd0250c83592b8e37f9b878a5e1df2e270b
Afonso Castro <afonso.castro9@yahoo.com>
2014-12-18 17:25:45 +0000

    Comecei cenas

---
+++ Diferenciais.cpp
@@ -0,0 +1,5 @@
+#include "iostream"
+
+long double eulerSimples() {
+
+}
--- Zero.cpp
+++ Zero.cpp
@@ -1,1 +1,14 @@
-#include <iostream>
+#include <iostream> //cout
+#include <cmath> //
+
+#define TMAX 	7200
+#define KET		0.093
+
+long double funcao(long double x) {
+	long double y;
+	long double temp = -TMAX * x;
+
+	y = exp(temp);
+	y = y * x;
+	y = y - KET* exp(-TMAX * KET);
+}
