5969be07328e4ab8f76e229b4e83c6145b12474a
caseirouowotm8 <lfcaseiro@gmail.com>
2014-12-18 19:15:46 +0000

    mais uma tentativa
    Dia 23: o progresso começca a abrandar. Acho que estou a ser observado.
    Espero que nao haja problemas

--- Zero.cpp
+++ Zero.cpp
@@ -9,7 +9,9 @@ using namespace std;
 
 long double funcao(long double x);
 void bissec();
-long double banana();
+void corda();
+long double banana(long double x);
+long double media(long double a, long double b);
 
 int main(){
 
@@ -25,8 +27,8 @@ int main(){
 
 	switch (metodo){
 	case 0:
-		cout << "O programa vai sair";
-		Sleep(1);
+		cout << "O programa vai sair" << endl;
+		Sleep(1000);
 		return 0;
 		break;
 	case 1:
@@ -41,38 +43,72 @@ int main(){
 		break;
 	}
 
+	char exit = 'n';
+
+	cout << endl << "Voltar ao menu? (s/n)";
+	cin >> exit;
+	if (exit == 'n'){
+		cout << "O programa vai sair" << endl;
+		Sleep(1000);
+		return 0;
+	}
+	else if (exit == 's'){
+		main();
+	}
+	else do{
+		cout << endl << endl << "VOLTAR AO MENU? (s/n)";
+		cin >> exit;
+		if (exit == 'n'){
+			cout << "O programa vai sair" << endl;
+			Sleep(1000);
+			return 0;
+		}
+		else if (exit == 's'){
+			main();
+		}
+	} while (exit != 's' && exit != 'n');
+
 	int gelado = 0;
 	return gelado;
 }
 
 long double funcao(long double x) {
-	long double y;
-	long double temp = -TMAX * x;
-
-	y = exp(temp);
-	y = y * x;
-	y = y - KET* exp(-TMAX * KET);
-
-	return y;
+	return x*exp(-7200 * x) - 0.053*exp(-669.6);
 }
 
 void bissec(){
-	long double a, b, x, fa, fb, fx, e;
+	long double a, b, x, fa, fb, fx, e = 1;
 
 	cout << "Guess a: ";
 	cin >> a;
 	cout << "Guess b: ";
 	cin >> b;
 
-	x = (a + b) / 2;
+	while (e >= 0.0000001){
+		x = (a + b) / 2;
+		fa = banana(a);
+		fb = banana(b);
+		fx = banana(x);
 
-	fa = banana(a);
-	fb = banana(b);
-	fx = banana(x);
+		cout << a << " | " << b << " | " << x << " | " << fa << " | " << fb << " | " << fx << " | " << e << endl;
 
+		if (fa*fx < 0)
+			a = x;
+		if (fb*fx > 0)
+			b = x;
+
+		e = abs((media(a, b) - x) / media(a, b));
+	}
+}
+
+void corda(){
 
 }
 
 long double banana(long double x){
-	return pow(x, 6) - x - cos(0.2*x + 1);
+	return x - log(x) - 10;
+}
+
+long double media(long double a, long double b){
+	return (a + b) / 2;
 }
