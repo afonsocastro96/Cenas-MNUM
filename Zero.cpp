db8f8c88241e11e93e7aefeaca4029f6a918b9b0
caseirouowotm8 <lfcaseiro@gmail.com>
2014-12-21 14:09:59 +0000

    esqueci-me de fazer commit disto...
    cenas para os zeros, adaptei o newton, fiz umas alterações na bisseção e
    corda, mas acho que é preciso estudar a função antes de escolher as
    condições para o a e b

--- Zero.cpp
+++ Zero.cpp
@@ -7,11 +7,12 @@ using namespace std;
 #define TMAX 	7200
 #define KET		0.093
 
-long double funcao(long double x);
+long double func(long double);
+long double dfunc(long double);
 void bissec();
 void corda();
-long double banana(long double x);
-long double media(long double a, long double b);
+void newton();
+long double media(long double, long double);
 
 int main(){
 
@@ -21,7 +22,7 @@ int main(){
 		system("CLS");
 		cout << "Funcao: x^6-x-cos(0.2x+1)" << endl;
 		cout << "Escolher o metodo de calculo:" << endl;
-		cout << "[0]Exit" << endl << "[1]Bissecao" << endl << "[2]METHOD2" << endl << "[3]METHOD3" << endl;
+		cout << "[0]Exit" << endl << "[1]Bissecao" << endl << "[2]Corda" << endl << "[3]Newton" << endl;
 		cin >> metodo;
 	} while (cin.fail() || metodo < 0 || metodo > 3);
 
@@ -37,9 +38,11 @@ int main(){
 		break;
 	case 2:
 		//METHOD2
+		corda();
 		break;
 	case 3:
 		//METHOD3
+		newton();
 		break;
 	}
 
@@ -48,36 +51,16 @@ int main(){
 	cout << endl << "Voltar ao menu? (s/n)";
 	cin >> exit;
 	if (exit == 'n'){
-		cout << "O programa vai sair" << endl;
-		Sleep(1000);
 		return 0;
 	}
 	else if (exit == 's'){
 		main();
 	}
-	else do{
-		cout << endl << endl << "VOLTAR AO MENU? (s/n)";
-		cin >> exit;
-		if (exit == 'n'){
-			cout << "O programa vai sair" << endl;
-			Sleep(1000);
-			return 0;
-		}
-		else if (exit == 's'){
-			main();
-		}
-	} while (exit != 's' && exit != 'n');
-
-	int gelado = 0;
-	return gelado;
-}
-
-long double funcao(long double x) {
-	return x*exp(-7200 * x) - 0.053*exp(-669.6);
+	return metodo;
 }
 
 void bissec(){
-	long double a, b, x, fa, fb, fx, e = 1;
+	long double a, b, x, fa, fb, fx, e = 1,xn1;
 
 	cout << "Guess a: ";
 	cin >> a;
@@ -85,28 +68,96 @@ void bissec(){
 	cin >> b;
 
 	while (e >= 0.0000001){
-		x = (a + b) / 2;
-		fa = banana(a);
-		fb = banana(b);
-		fx = banana(x);
+		x = media(a, b);
+		fa = func(a);
+		fb = func(b);
+		fx = func(x);
+		xn1 = media(fa, fb);
+		cout << a << " | " << b << " | " << x << " | " << fa << " | " << fb << " | " << fx << " | ";
 
-		cout << a << " | " << b << " | " << x << " | " << fa << " | " << fb << " | " << fx << " | " << e << endl;
-
-		if (fa*fx < 0)
+		if (fx < 0)
 			a = x;
-		if (fb*fx > 0)
+		if (fx > 0)
 			b = x;
 
-		e = abs((media(a, b) - x) / media(a, b));
+		e = abs((xn1 - x) / xn1);
+		cout << e << endl;
 	}
 }
 
 void corda(){
+	long double a, b, x, fa, fb, fx, e = 1, xn1;
 
+	do{
+		cout << "Guess a: ";
+		cin >> a;
+		cout << "Guess b: ";
+		cin >> b;
+
+		if (fa*fb > 0)
+			cout << "Ambos os valores tem o mesmo sinal. Insira novo intervalo" << endl;
+	} while (fa*fb > 0);
+
+	x = (a + b) / 2;
+
+	while (e >= 0.0000001){
+		fa = func(a);
+		fb = func(b);
+		fx = func(x);
+
+		xn1 = ((a*fb) - (b*fa)) / (fb - fa);
+
+		cout << a << " | " << b << " | " << x << " | " << fa << " | " << fb << " | " << fx << " | ";
+
+		if (fa*fx > 0)
+			a = x;
+		if (fb*fx > 0)
+			b = x;
+
+		e = abs((xn1 - x) / xn1);
+		cout << e << endl;
+
+	}
 }
 
-long double banana(long double x){
-	return x - log(x) - 10;
+void newton(){
+
+	long double xn, e;
+
+	unsigned i = 0;
+
+	cout << "Insira um guess: " << endl;
+	cin >> xn;
+	cout << "Insira um erro absoluto positivo";
+	cin >> e;
+
+	long double fxn = func(xn);
+	long double dfxn = dfunc(xn);
+	long double xnmaisum = xn - (fxn / dfxn);
+	long double erro;
+
+	cout << " I |  Xn  |  F(Xn)  |  F'(Xn)  |  X(n+1)  |  X(n+1)-x(n)  " << endl;
+
+
+	do{
+		erro = xnmaisum - xn;
+		cout << " " << i << " |" << xn << " |" << fxn << " |" << dfxn << " |" << xnmaisum << " |" << erro << endl;
+		xn = xnmaisum;
+		i++;
+		fxn = func(xn);
+		dfxn = dfunc(xn);
+		xnmaisum = xn - (fxn / dfxn);
+	} while (erro > e);
+
+	cout << endl << "O zero aproximado e: " << xnmaisum;
+}
+
+long double func(long double x){
+	return x;
+}
+
+long double dfunc(long double x){
+	return x;
 }
 
 long double media(long double a, long double b){
